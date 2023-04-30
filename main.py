43300180414025c7d5765878448a5c9869a45f90
rgimad <rustem.gimadutdinov@gmail.com>
2023-04-30 16:54:30 +0300

    add choose input image. TODO add scroll if image doesnt fit to label

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, QtGui, uic
 import sys
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('isr.ui', self)
-        self.show()
+
+        self.btnChooseImage.clicked.connect(self.choose_input_image)
+
+    def choose_input_image(self):
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files (*.png *.jpg *.bmp)")
+        if filename:
+            pixmap = QtGui.QPixmap(filename)
+            self.labelInputImage.setPixmap(pixmap)
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
 
