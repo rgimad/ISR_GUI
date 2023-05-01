c21d3668123d5483effac5b7bd33cc6cabee5ac6
rgimad <rustem.gimadutdinov@gmail.com>
2023-05-01 18:16:40 +0300

    added save output image feature, forming default name of file to save to

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, QtGui, uic
 from PyQt5.QtGui import QPixmap, QPalette
 from PyQt5.QtWidgets import QApplication, QMainWindow, QScrollArea, QFileDialog, QLabel
 import sys
+import os
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,17 +12,32 @@ class MainWindow(QMainWindow):
         self.labelOutputImage = QLabel()
 
         self.btnChooseImage.clicked.connect(self.choose_input_image)
+        self.btnSaveResult.clicked.connect(self.save_output_image)
+
+        self.input_image_filename = None
 
     def choose_input_image(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files (*.png *.jpg *.bmp)")
         if filename:
+            # print(filename)
+            self.input_image_filename = filename
             pixmap = QPixmap(filename)
             self.labelInputImage.setPixmap(pixmap)
             self.scrollAreaInputImage.setWidget(self.labelInputImage)
             self.labelOutputImage.clear()
 
-            # self.labelOutputImage.setPixmap(pixmap)
-            # self.scrollAreaOutputImage.setWidget(self.labelOutputImage)
+            # tmp, for debug:
+            self.labelOutputImage.setPixmap(pixmap)
+            self.scrollAreaOutputImage.setWidget(self.labelOutputImage)
+
+    def save_output_image(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save Image", os.path.basename(os.path.splitext(self.input_image_filename)[0]) + "_SR_" + self.cbChooseModel.currentText(), "Image Files (*.png *.jpg *.bmp)")
+        out_pixmap = self.labelOutputImage.pixmap()
+        if out_pixmap is not None and filename:
+            out_pixmap.save(filename)
+
+
+
 
 
 
